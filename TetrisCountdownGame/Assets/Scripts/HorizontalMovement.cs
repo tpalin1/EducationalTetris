622fb5859c627241a5bc9cd732bb94dc58eec0e2
Tom <tpalin57@gmail.com>
2024-02-21 19:23:28 +0000

    feat: Added the border around the grid

--- TetrisCountdownGame/Assets/Scripts/HorizontalMovement.cs
+++ TetrisCountdownGame/Assets/Scripts/HorizontalMovement.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 public class HorizontalMovement : MonoBehaviour
-{
+{   
+    private float previousTime;
      public float fallSpeed = 1.0f; // Speed at which the block falls. Adjust as needed.
      private float timer = 0f;
      public Vector3 rotationPoint;
     public SpawnerForObjects spawner; // Reference to the spawner script. Set this in the Unity editor.
 
 
-
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,17 +24,18 @@ public class HorizontalMovement : MonoBehaviour
     void Update()
     {
 
-         // Make the block fall down constantly
-        transform.position += new Vector3(0, -fallSpeed * Time.deltaTime, 0);
+        
 
         timer += Time.deltaTime;
 
-        if(timer>=10){
-            FindObjectOfType<SpawnerForObjects>().SpawnBlock();
-            this.enabled = false;
-            timer = 0; // Reset the time
+        if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow)? fallSpeed /10 : fallSpeed)){
+            transform.position += new Vector3(0, -1, 0);
+            if(!validMove()){
+                transform.position -= new Vector3(0,-1, 0);
+                
+            }
+            previousTime = Time.time;
         }
-
 
 
 
@@ -42,11 +45,17 @@ public class HorizontalMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
+            if(!validMove()){
+                transform.position -= new Vector3(-1, 0, 0);
+            }
         }
         //If they move arrow right, move the blocks right 1 space
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
+            if(!validMove()){
+                transform.position -= new Vector3(1, 0, 0);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
@@ -58,6 +67,38 @@ public class HorizontalMovement : MonoBehaviour
     {
         Vector3 pivot = GetComponent<SpriteRenderer>().bounds.center;
         transform.RotateAround(pivot, Vector3.forward, 90f);
+    }
+
+    //Allow block collisions so that they can stack on one another
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Block")
+        {
+            FindObjectOfType<SpawnerForObjects>().SpawnBlock();
+            Debug.Log("Block has collided with another block");
+            this.enabled = false;
+        }
+    }
+
+   //Do is valid move for the bottom and the left and right
+    bool validMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            Debug.Log("This is x" + roundedX);
+            Debug.Log("This is Y"+ roundedY);
+           
+            if (roundedX < -7 || roundedX >= gridWidth+6 || roundedY <-15)
+            {
+                Debug.Log("Invalid move");
+                return false;
+            }
+        }
+        return true;
     }
 }
 
