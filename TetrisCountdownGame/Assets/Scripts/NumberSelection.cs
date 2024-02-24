bbe377276de177186c01d96eac5bf59db37fd0ed
Tom <tpalin57@gmail.com>
2024-02-24 01:03:34 +0000

    feat: Added more to the canvas, and added the ability to see the equations being typed out

--- TetrisCountdownGame/Assets/Scripts/NumberSelection.cs
+++ TetrisCountdownGame/Assets/Scripts/NumberSelection.cs
@@ -9,12 +9,18 @@ public class ProblemSetController : MonoBehaviour
     private ProblemSet problemSet;
 
 
-  public Transform buttonContainer; // Add this line
+   public Transform numberButtonContainer; // Container for number buttons
+    public Transform operatorButtonContainer; // Container for operator buttons
+
+    public TextMeshProUGUI equationContainer; // Container for the equation
     public Button buttonPrefab; // Add this line
+
+     public TextMeshProUGUI targetNumberText; // Add this line
 
 
 
     
+    public List<ProblemSelectable> userSelectables = new List<ProblemSelectable>(); // 
 
 
     private int currentLevel = 1;
@@ -23,36 +29,81 @@ public class ProblemSetController : MonoBehaviour
     
    void Start()
     {
+
         problemSet = new ProblemSet();
+
         var newProblem = problemSet.GetNewProblemSet(currentLevel);
+
         Debug.Log("Target Number: " + newProblem.Item1);
         Debug.Log("Selectables: " + string.Join(", ", newProblem.Item2));
 
-        // Create a button for each number selectable
+        // Set the target number text
+        targetNumberText.text = "Target Number: " + newProblem.Item1; // Add this line
+
+        
+
+       // Create a button for each number selectable
         foreach (var selectable in newProblem.Item2)
         {
-           if (selectable is Number number)
+            if (selectable is Number number)
             {
-                Button button = Instantiate(buttonPrefab, buttonContainer.transform);
-                string buttonText = number.ToString().Split('=')[1].Trim(' ', '}'); // Add this line
-                button.GetComponentInChildren<TextMeshProUGUI>().text = buttonText; // Change this line
-
-                // Add a click listener to the button
+                Button button = Instantiate(buttonPrefab, numberButtonContainer.transform);
+                string buttonText = number.ToString().Split('=')[1].Trim(' ', '}');
+                button.GetComponentInChildren<TextMeshProUGUI>().text = buttonText;
+                button.onClick.AddListener(() => OnButtonClicked(selectable));
+            }
+            else if (selectable is BinaryOperator binaryOperator)
+            {
+                Button button = Instantiate(buttonPrefab, operatorButtonContainer.transform);
+                string buttonText = "";
+                switch (binaryOperator.BinOp)
+                {
+                    case BinaryOperatorType.Addition:
+                        buttonText = "+";
+                        break;
+                    case BinaryOperatorType.Subtraction:
+                        buttonText = "-";
+                        break;
+                    // Add cases for other binary operators here
+                }
+                button.GetComponentInChildren<TextMeshProUGUI>().text = buttonText;
                 button.onClick.AddListener(() => OnButtonClicked(selectable));
             }
         }
+        
     }
 
      // This method is called when a button is clicked
-    void OnButtonClicked(ProblemSelectable selectable)
+   // This method is called when a button is clicked
+// This method is called when a button is clicked
+void OnButtonClicked(ProblemSelectable selectable)
+{
+    Debug.Log("Button clicked: " + selectable);
+    userSelectables.Add(selectable);
+
+    // Add the selected button to the equation
+    if (selectable is Number number)
     {
-        Debug.Log("Button clicked: " + selectable);
-        // Here you can add the code to handle the button click
+        equationContainer.text += number.Value.ToString() + " "; // Append number to the equation
     }
+    else if (selectable is BinaryOperator binaryOperator)
+    {
+        switch (binaryOperator.BinOp)
+        {
+            case BinaryOperatorType.Addition:
+                equationContainer.text += "+ "; // Append operator to the equation
+                break;
+            case BinaryOperatorType.Subtraction:
+                equationContainer.text += "- "; // Append operator to the equation
+                break;
+            // Add cases for other binary operators here
+        }
+    }
+}
 
 
-    // Call this method when the user submits their solution
-    public void CheckSolution(List<ProblemSelectable> userSelectables)
+   // Call this method when the user submits their solution
+    public void CheckSolution()
     {
         if (problemSet.IsSolutionCorrect(userSelectables))
         {
@@ -64,7 +115,7 @@ public class ProblemSetController : MonoBehaviour
             Debug.Log("Incorrect solution.");
         }
 
-     
+        
     }
 
     
